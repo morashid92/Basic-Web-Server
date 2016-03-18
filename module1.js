ecab7dbc600e03fca436a62bc665c1627402ecda
Mohammed Rashid <rashid_92@hotmail.co.uk>
2016-03-18 03:43:52 +0000

    built the webserver

--- module1.js
+++ module1.js
@@ -1,4 +1,9 @@
-var _ = require('underscore');
-var m2 = require('./module2');
+var http = require('http');
 
-console.log(_);
+var server = http.createServer(function(request, response) {
+	console.log('got a request!');
+	response.write('hi');
+	response.end();
+});
+
+server.listen(3000);
